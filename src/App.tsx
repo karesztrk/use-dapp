00e1e81244d840148c2f4f98357327f86c5a15ba
Károly Török <torok.karoly.krisztian@gmail.com>
2021-06-05 14:53:24 +0200

    Minero UI improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { useEtherBalance, useEthers } from '@usedapp/core';
 import { formatEther } from '@ethersproject/units';
@@ -8,34 +8,55 @@ import {
   Divider,
   Flex,
   Heading,
+  HStack,
   Text,
   VStack,
 } from '@chakra-ui/layout';
 import { Button } from '@chakra-ui/button';
 import { Stat, StatHelpText, StatLabel, StatNumber } from '@chakra-ui/stat';
-
+import { Spinner } from '@chakra-ui/spinner';
+import Logo from './Logo';
 const App = () => {
-  const { activateBrowserWallet, account } = useEthers();
+  const [connecting, setConnecting] = useState(false);
+  const { activateBrowserWallet, account, deactivate } = useEthers();
   const etherBalance = useEtherBalance(account);
+
+  const onConnectClick = () => {
+    activateBrowserWallet();
+    setConnecting(true);
+  };
 
   return (
     <Flex minHeight="100vh" direction="column">
-      <header></header>
+      <HStack
+        as="header"
+        p={5}
+        mb={5}
+        bgGradient="linear(to-l, #7928CA, #FF0080)"
+        color="white"
+      >
+        <Logo />
+        <Heading>useDapp</Heading>
+      </HStack>
       <Box as="main" overflow="hidden">
         <Container>
           <VStack spacing={4} alignItems="flex-start">
-            <div>
-              <Button onClick={() => activateBrowserWallet()}>Connect</Button>
-            </div>
-            {account && <Text fontWeight={600}>Account {account}</Text>}
+            {account && (
+              <>
+                <Button onClick={() => deactivate()}>Disconnect</Button>
+                <Text fontWeight={600}>Account {account}</Text>
+              </>
+            )}
+            {!account && <Button onClick={onConnectClick}>Connect</Button>}
             <Divider></Divider>
             {etherBalance && (
               <Stat>
-                <StatLabel>Balance</StatLabel>
+                <StatLabel>Ether Balance</StatLabel>
                 <StatNumber>{formatEther(etherBalance)}</StatNumber>
                 <StatHelpText>{new Date().toLocaleTimeString()}</StatHelpText>
               </Stat>
             )}
+            {!etherBalance && connecting && <Spinner></Spinner>}
           </VStack>
         </Container>
       </Box>
